75807f268cd22ca9770f3d29f0ea7784eaec6f07
Iago <iagonobre22@gmail.com>
2020-11-15 17:15:51 -0300

    feat: adicionado formulário de Cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -7,6 +7,7 @@ import Header from '../../components/Header';
 import Title from '../../components/Title';
 
 import ArrowRight from '../../assets/arrowright.svg';
+import PickerIcon from '../../assets/miniarrowdown.svg';
 
 import {
   Container,
@@ -17,48 +18,171 @@ import {
   InputArea,
   ContainerInput,
   TitleInput,
+  Recommend,
+  RecommendPicker,
+  TextRecommend,
+  NumberRecommend,
+  PickerContainer,
+  PickerArea,
+  PickerText,
+  PickerOption,
+  Picker,
 } from './styles';
 
 const Categoria: React.FC = () => {
-  const [complete, setComplete] = useState(false);
+  const [nameFocus, setNameFocus] = useState(false);
+  const [numberFocus, setNumberFocus] = useState(false);
+  const [openOptionsSexo, setOpenOptionsSexo] = useState(false);
+  const [openOptionsIdade, setOpenOptionsIdade] = useState(false);
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [sexo, setSexo] = useState('');
+  const [idade, setIdade] = useState('');
   const { navigate } = useNavigation();
 
   const handleNavigateToCategoria = useCallback(() => {
     navigate('Categoria');
   }, [navigate]);
 
+  const handleCompleteForm = useCallback(() => {
+    setNumber('(84) 8498-5522');
+    setName('Iago Nobre Silva');
+    setSexo('Masculino');
+    setIdade('16 anos');
+  }, []);
+
+  const handleOpenOptionsIdade = useCallback(() => {
+    setOpenOptionsIdade(!openOptionsIdade);
+  }, [openOptionsIdade]);
+
+  const handleOpenOptionsSexo = useCallback(() => {
+    setOpenOptionsSexo(!openOptionsSexo);
+  }, [openOptionsSexo]);
+
   return (
-    <Container>
+    <>
       <Header />
-      <Title>
-        Cadastre um
-        {' \n'}
-        paciente:
-      </Title>
-      <CategoryContainer>
-        <KeyboardAvoidingView behavior="position" style={{ flex: 1 }}>
-          <InputArea style={{ marginTop: 0 }}>
-            <TitleInput>Telefone do Paciente</TitleInput>
-            <ContainerInput placeholder="Buscar pelo Uber" />
-          </InputArea>
-          <InputArea>
-            <TitleInput>Nome do Paciente</TitleInput>
-            <ContainerInput placeholder="Buscar pelo Uber" />
-          </InputArea>
-          {complete ? (
-            <SimpleButtonDisable>
-              <ButtonText>Cadastrar</ButtonText>
-              <ArrowRight style={{ position: 'absolute', right: '5%' }} />
-            </SimpleButtonDisable>
-          ) : (
-            <SimpleButton onPress={handleNavigateToCategoria}>
-              <ButtonText>Cadastrar</ButtonText>
-              <ArrowRight style={{ position: 'absolute', right: '5%' }} />
-            </SimpleButton>
-          )}
-        </KeyboardAvoidingView>
-      </CategoryContainer>
-    </Container>
+      <Container>
+        <Title>
+          Cadastre um
+          {' \n'}
+          paciente:
+        </Title>
+        <CategoryContainer>
+          <KeyboardAvoidingView behavior="position" style={{ flex: 1 }}>
+            <InputArea style={{ marginTop: 0 }}>
+              <TitleInput>Telefone do Paciente</TitleInput>
+              <ContainerInput
+                focused={numberFocus}
+                onFocus={() => setNumberFocus(true)}
+                onBlur={() => setNumberFocus(false)}
+                onChangeText={setNumber}
+                value={number}
+                placeholder="Buscar pelo Uber"
+              />
+              {number === '8' && (
+                <>
+                  <Recommend onPress={handleCompleteForm}>
+                    <TextRecommend>Iago Nobre Silva</TextRecommend>
+                    <NumberRecommend>(84) 8498-5522</NumberRecommend>
+                  </Recommend>
+                  <Recommend>
+                    <TextRecommend>Maria Eduarda Lima da Luz</TextRecommend>
+                    <NumberRecommend>(84) 8002-8922</NumberRecommend>
+                  </Recommend>
+                </>
+              )}
+            </InputArea>
+            <InputArea>
+              <TitleInput>Nome do Paciente</TitleInput>
+              <ContainerInput
+                onFocus={() => setNameFocus(true)}
+                onBlur={() => setNameFocus(false)}
+                focused={nameFocus}
+                value={name}
+                onChangeText={setName}
+                placeholder="Nome do Paciente"
+              />
+            </InputArea>
+            <PickerContainer>
+              <PickerArea>
+                <PickerText>Sexo</PickerText>
+                <Picker onPress={handleOpenOptionsSexo}>
+                  <PickerOption>{sexo || 'Selecione'}</PickerOption>
+                  <PickerIcon />
+                </Picker>
+                {openOptionsSexo && (
+                  <>
+                    <RecommendPicker
+                      onPress={() => {
+                        setSexo('Feminino');
+                        setOpenOptionsSexo(false);
+                      }}
+                    >
+                      <TextRecommend>Feminino</TextRecommend>
+                    </RecommendPicker>
+                    <RecommendPicker
+                      onPress={() => {
+                        setSexo('Masculino');
+                        setOpenOptionsSexo(false);
+                      }}
+                    >
+                      <TextRecommend>Masculino</TextRecommend>
+                    </RecommendPicker>
+                  </>
+                )}
+              </PickerArea>
+              <PickerArea>
+                <PickerText>Idade</PickerText>
+                <Picker onPress={handleOpenOptionsIdade}>
+                  <PickerOption>{idade || 'Selecione'}</PickerOption>
+                  <PickerIcon />
+                </Picker>
+                {openOptionsIdade && (
+                  <>
+                    <RecommendPicker
+                      onPress={() => {
+                        setIdade('1-19');
+                        setOpenOptionsIdade(false);
+                      }}
+                    >
+                      <TextRecommend>1-19 anos</TextRecommend>
+                    </RecommendPicker>
+                    <RecommendPicker
+                      onPress={() => {
+                        setIdade('20-59');
+                        setOpenOptionsIdade(false);
+                      }}
+                    >
+                      <TextRecommend>20-59 anos</TextRecommend>
+                    </RecommendPicker>
+                    <RecommendPicker
+                      onPress={() => {
+                        setIdade('60+');
+                        setOpenOptionsIdade(false);
+                      }}
+                    >
+                      <TextRecommend>60+ anos</TextRecommend>
+                    </RecommendPicker>
+                  </>
+                )}
+              </PickerArea>
+            </PickerContainer>
+            {!sexo && !number && !name && !idade ? (
+              <SimpleButtonDisable>
+                <ButtonText>Cadastrar</ButtonText>
+                <ArrowRight style={{ position: 'absolute', right: '5%' }} />
+              </SimpleButtonDisable>
+            ) : (
+              <SimpleButton onPress={handleNavigateToCategoria}>
+                <ButtonText>Cadastrar</ButtonText>
+                <ArrowRight style={{ position: 'absolute', right: '5%' }} />
+              </SimpleButton>
+            )}
+          </KeyboardAvoidingView>
+        </CategoryContainer>
+      </Container>
+    </>
   );
 };
 
--- src/pages/Cadastro/styles.ts
+++ src/pages/Cadastro/styles.ts
@@ -1,7 +1,11 @@
 import { RectButton } from 'react-native-gesture-handler';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View`
+interface InputProps {
+  focused: boolean;
+}
+
+export const Container = styled.ScrollView`
   flex: 1;
   background-color: #ededed;
 `;
@@ -10,6 +14,7 @@ export const CategoryContainer = styled.View`
   background-color: #ffffff;
   align-items: center;
   padding-top: 48px;
+  padding-bottom: 48px;
   width: 100%;
   margin-top: 28px;
   flex: 1;
@@ -25,12 +30,19 @@ export const TitleInput = styled.Text`
   color: #000000;
 `;
 
-export const ContainerInput = styled.TextInput`
+export const ContainerInput = styled.TextInput<InputProps>`
   margin-top: 8px;
   width: 308px;
   height: 38px;
   background-color: #eeeeee;
   padding: 8px;
+
+  ${props =>
+    props.focused
+      ? css`
+          border: 2px solid #000;
+        `
+      : null}
 `;
 
 export const SimpleButton = styled(RectButton)`
@@ -51,7 +63,7 @@ export const ButtonText = styled.Text`
 `;
 
 export const SimpleButtonDisable = styled.View`
-  width: 298px;
+  width: 308px;
   height: 54px;
   margin-top: 28px;
   background-color: #acacac;
@@ -59,4 +71,67 @@ export const SimpleButtonDisable = styled.View`
   align-items: center;
   justify-content: center;
   flex-direction: row;
+`;
+
+export const Recommend = styled.TouchableOpacity`
+  width: 308px;
+  height: 34px;
+  background-color: #fafafa;
+  padding: 8px;
+  justify-content: center;
+  border: 1px solid #eeeeee;
+`;
+
+export const RecommendPicker = styled.TouchableOpacity`
+  width: 128px;
+  height: 34px;
+  background-color: #fafafa;
+  padding: 8px;
+  justify-content: center;
+  border: 1px solid #eeeeee;
+`;
+
+export const TextRecommend = styled.Text`
+  color: #757575;
+  font-size: 12px;
+  font-family: 'UberMoveText Medium';
+`;
+
+export const NumberRecommend = styled.Text`
+  color: #757575;
+  font-size: 10px;
+  font-family: 'UberMoveText Regular';
+`;
+
+export const PickerContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 308px;
+`;
+
+export const PickerArea = styled.View`
+  margin-top: 18px;
+`;
+
+export const PickerText = styled.Text`
+  font-family: 'UberMoveText Medium';
+  font-size: 14px;
+  color: #000000;
+`;
+
+export const PickerOption = styled.Text`
+  font-family: 'UberMoveText Regular';
+  font-size: 14px;
+  color: #000000;
+`;
+
+export const Picker = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 8px;
+  width: 128px;
+  height: 38px;
+  background-color: #eeeeee;
+  padding: 8px;
 `;
